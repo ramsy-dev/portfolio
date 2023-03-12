b59a519bc6ce7a7fe2b7962d5b3eb8a1599c7a48
Ramsy de Vos <ramsy@ramsy.dev>
2023-03-12 19:09:31 +0100

    chore: add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
-
 import solidJs from "@astrojs/solid-js";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [solidJs()]
+  integrations: [solidJs(), tailwind()]
 });
